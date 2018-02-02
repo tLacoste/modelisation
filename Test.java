f4bdb290133c082ab055d5a5d923027d092bfc10
Fornito Marvin <marvin.fornito@gmail.com>
2018-02-02 18:58:20 +0100

    Debug

--- Test.java
+++ Test.java
@@ -5,6 +5,17 @@ class Test
    static boolean visite[];
    
    /**
+    * Classe de lancement des méthodes
+    * @param args
+    */
+   public static void main(String[] args)
+	 {
+		testHeap();
+		testGraph();
+		testDijkstra();
+	 }
+   
+   /**
     * Methode de parcour des noeuds du graphe
     * @param g
     * 			Graphe parcouru
@@ -46,32 +57,6 @@ class Test
 	 }
    
    /**
-    * Test de la création d'un graph
-    */
-   public static void testGraph()
-	 {
-		int n = 5;
-		int i,j;
-		Graph g = new Graph(n*n+2);
-		
-		for (i = 0; i < n-1; i++)
-		  for (j = 0; j < n ; j++)
-			g.addEdge(new Edge(n*i+j, n*(i+1)+j, 1664 - (i+j)));
-
-		for (j = 0; j < n ; j++)		  
-		  g.addEdge(new Edge(n*(n-1)+j, n*n, 666));
-		
-		for (j = 0; j < n ; j++)					
-		  g.addEdge(new Edge(n*n+1, j, 0));
-		
-		g.addEdge(new Edge(13,17,1337));
-		g.writeFile("test.dot");
-		// dfs Ã  partir du sommet 3
-		visite = new boolean[n*n+2];
-		dfs(g, 3);
-	 }
-   
-   /**
     * Test de la méthode Dijsktra
     */
    public static void testDijkstra()
@@ -100,13 +85,28 @@ class Test
    }
    
    /**
-    * Classe de lancement des méthodes
-    * @param args
+    * Test de la création d'un graph
     */
-   public static void main(String[] args)
+   public static void testGraph()
 	 {
-		testHeap();
-		testGraph();
-		testDijkstra();
+		int n = 5;
+		int i,j;
+		Graph g = new Graph(n*n+2);
+		
+		for (i = 0; i < n-1; i++)
+		  for (j = 0; j < n ; j++)
+			g.addEdge(new Edge(n*i+j, n*(i+1)+j, 1664 - (i+j)));
+
+		for (j = 0; j < n ; j++)		  
+		  g.addEdge(new Edge(n*(n-1)+j, n*n, 666));
+		
+		for (j = 0; j < n ; j++)					
+		  g.addEdge(new Edge(n*n+1, j, 0));
+		
+		g.addEdge(new Edge(13,17,1337));
+		g.writeFile("test.dot");
+		// dfs Ã  partir du sommet 3
+		visite = new boolean[n*n+2];
+		dfs(g, 3);
 	 }
 }
